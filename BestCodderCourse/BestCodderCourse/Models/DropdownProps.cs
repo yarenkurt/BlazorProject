08c36e22406dacce433ae86a864a3c80c3461647
Yaren <yarenkurt078@outlook.com>
2022-08-23 12:20:28 +0300

    updated

---
+++ BestCodderCourse/BestCodderCourse/Models/DropdownProps.cs
@@ -0,0 +1,8 @@
+﻿namespace BestCodderCourse.Models;
+
+public class DropdownProps
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Value { get; set; }
+}
